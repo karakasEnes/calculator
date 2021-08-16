ed05097abc4a1347a606cd5e6eda78cd0bc0fa36
pyuser <enes.karakas.e@gmail.com>
2021-08-16 21:52:56 +0300

    v7 done, operator func, decimal func done

--- script.js
+++ script.js
@@ -2,24 +2,44 @@ const inputBtns = document.querySelectorAll("button");
 const calculatorDisplay = document.querySelector("h1");
 const clearBtn = document.getElementById("clear-btn");
 
-function sendNumberValue(number) {
-  const displayValue = calculatorDisplay.textContent;
+let firstValue = 0;
+let operatorValue = "";
+let awaitingSecondValue = false;
 
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+function sendNumberValue(number) {
+  if (awaitingSecondValue) {
+    calculatorDisplay.textContent = number;
+    awaitingSecondValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
+  if (awaitingSecondValue) {
+    return;
+  }
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function operatorSetup(operator) {
+  if (!firstValue) {
+    firstValue = Number(calculatorDisplay.textContent);
+  } else {
+  }
+  awaitingSecondValue = true;
+  operatorValue = operator;
 }
 
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("operator")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => operatorSetup(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
     inputBtn.addEventListener("click", () => addDecimal());
   }
@@ -27,6 +47,9 @@ inputBtns.forEach((inputBtn) => {
 
 function resetAll() {
   calculatorDisplay.textContent = "0";
+  firstValue = 0;
+  operatorValue = "";
+  awaitingSecondValue = false;
 }
 
 clearBtn.addEventListener("click", () => resetAll());
